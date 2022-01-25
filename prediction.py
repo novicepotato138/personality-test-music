f7b982d0e7d7c53702f61f45d9ac8b80b95b4fcb
J W <jwooon03+heroku@protonmail.com>
2022-01-25 22:40:30 +0800

    added categorical data details

--- prediction.py
+++ prediction.py
@@ -48,10 +48,10 @@ def generate_X1(params):
 
     txtFile.write("bluegrass,blues,classical,country,dance,folk,funk,gospel,heavymetal,world,jazz,newage,oldies,opera,pop,punk,rap,reggae,religious,rock,rnb,soundtracks,Mellow,Unpretentious,Sophisticated,Intense,Contemporary,location_asia,location_australia,location_europe,location_malaysia,location_north_america,location_south_america,gender_Female,gender_Male,gender_Prefer not to say,isMusician_No,isMusician_Yes,age_18 - 24,age_25 - 34,age_35 - 44,age_45 - 54,age_Above 55,age_Under 18\n")
     arrangement = ','.join([qc2,qc3,qc4,qc5,qc6,qc7,qc8,qc9,qc10,qc11,qc12,qc13,qc14,qc15,qc16,qc17,qc18,qc19,qc20,qc21,qc22,qc23,str(mellow),str(unpretentious),str(sophisticated),str(intense),str(contemporary),
-    "1" if location == "Asia" else "0", "1" if location == "Australia" else "0", "1" if location == "Europe" else "0", "1" if location == "Malaysia" else "0", "1" if location == "XXX" else ""0"", 
-    gender,
-    isMusician,
-    age
+    "1" if location == "Asia (Other than Malaysia)" else "0", "1" if location == "Australia" else "0", "1" if location == "Europe" else "0", "1" if location == "Malaysia" else "0", "1" if location == "North America" else "0", "1" if location == "South America" else "0",
+    "1" if gender == "Female" else "0", "1" if gender == "Male" else "0", "1" if gender == "Prefer not to say" else "0"
+    "1" if isMusician == "No" else "0", "1" if isMusician == "Yes" else "0"
+    "1" if age == "18 - 24" else "0", "1" if age == "25 - 34" else "0", "1" if age == "35 - 44" else "0", "1" if age == "45 - 54" else "0", "1" if age == "Above 55" else "0", "1" if age == "Under 18" else "0"
     ])
     txtFile.write(arrangement)
     txtFile.close()
